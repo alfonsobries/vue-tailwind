fd10e24dd54b90fa4492ac49b8a4782fab2f6b47
alfonsobries <alfonsobries@gmail.com>
2019-04-06 10:31:11 -0600

    Eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,8 @@ module.exports = {
     node: true
   },
   'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
+    'plugin:vue/recommended'
+    // '@vue/standard'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
