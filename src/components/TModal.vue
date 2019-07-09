50d62a545cc19d3a8debe521c8bdf50de44c75e9
alfonsobries <alfonsobries@gmail.com>
2019-07-09 09:18:10 -0500

    Minor fixes

--- src/components/TModal.vue
+++ src/components/TModal.vue
@@ -29,7 +29,7 @@
                 viewBox="0 0 20 20"
               >
                 <title>close</title>
-                <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
+              <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
             </slot>
           </button>
 
@@ -221,22 +221,19 @@ export default {
     value (value) {
       this.localShow = value
     },
-    localShow: {
-      async handler(shown) {
-        this.$emit('input', shown);
-        this.$emit('change', shown);
+    async localShow(shown) {
+      this.$emit('input', shown);
+      this.$emit('change', shown);
 
-        if (shown) {
-          this.beforeOpen();
-          await this.$nextTick();
-          this.opened();
-        } else {
-          this.beforeClose();
-          await this.$nextTick();
-          this.closed();
-        }
-      },
-      immediate: true
+      if (shown) {
+        this.beforeOpen();
+        await this.$nextTick();
+        this.opened();
+      } else {
+        this.beforeClose();
+        await this.$nextTick();
+        this.closed();
+      }
     }, 
   },
 
