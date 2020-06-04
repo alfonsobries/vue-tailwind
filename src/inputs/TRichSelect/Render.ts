f0292923ce721165672cb05b8594f51e361a9f36
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-06-04 10:30:06 -0500

    Icon for the select button and better styling

--- src/inputs/TRichSelect/Render.ts
+++ src/inputs/TRichSelect/Render.ts
@@ -89,7 +89,45 @@ export default class TRichSelectRender {
           },
         },
       },
+      [
+        this.createSelectButtonLabel(),
+        this.createSelectButtonIcon(),
+      ],
+    );
+  }
+
+  createSelectButtonLabel(): VNode {
+    return this.createElement(
+      'span',
+      {
+        ref: 'selectButtonLabel',
+        class: this.component.getElementCssClass('selectButtonLabel'),
+      },
       this.component.value,
+    );
+  }
+
+  createSelectButtonIcon(): VNode {
+    return this.createElement(
+      'svg',
+      {
+        ref: 'selectButtonIcon',
+        attrs: {
+          fill: 'currentColor',
+          xmlns: 'http://www.w3.org/2000/svg',
+          viewBox: '0 0 20 20',
+        },
+        class: this.component.getElementCssClass('selectButtonIcon'),
+      },
+      [
+        this.createElement('path', {
+          attrs: {
+            'clip-rule': 'evenodd',
+            'fill-rule': 'evenodd',
+            d: 'M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z',
+          },
+        }),
+      ],
     );
   }
 
