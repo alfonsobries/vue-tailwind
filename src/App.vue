0e5612487995689b797270329edb96167378baf1
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-07-20 10:55:12 -0500

    Ttable demo

--- src/App.vue
+++ src/App.vue
@@ -455,6 +455,209 @@
       :total-items="9999"
       :value="1"
     />
+
+    <t-table
+      table-class="w-full rounded-lg overflow-hidden table border-blue-700 border"
+      :thead-class="{
+        thead: '',
+        tr: 'border-blue-700',
+        th: 'uppercase font-bold p-2 bg-blue-500 text-blue-900 text-sm text-shadow',
+      }"
+      :tbody-class="{
+        tbody: '',
+        tr: 'border-t-0',
+        td: 'p-2 text-sm text-center bg-blue-400 text-blue-900',
+      }"
+      :headers="['Name', 'Email', 'Age', 'Sales']"
+      :data="[
+        ['Alfonso Bribiesca', 'alfonso@vexilo.com', '31', '$9,999.00'],
+        ['Saida Redondo', 'saida@gmail.com', 27, '$124.00'],
+        ['Regina Bribiesca', 'regina@gmail.com', 1, '$0.00']
+      ]"
+    />
+
+    <t-table
+      :headers="['Name', 'E-mail', 'Status', '']"
+      :data="[
+        {
+          id: 1,
+          name: 'Alfonso Bribiesca',
+          email: 'alfonso@vexilo.com',
+          is_approved: true,
+        },
+        {
+          id: 2,
+          name: 'Saida Redondo',
+          email: 'saida@gmail.com',
+          is_approved: false,
+        },
+      ]"
+      :responsive="true"
+      :responsive-breakpoint="1024"
+      :tbody-class="{ tbody: 'border-t lg:border-0', tr: 'border-0 lg:border-t', td: 'p-3' }"
+    >
+      <template
+        slot="tbody"
+        slot-scope="{ tbodyClass, trClass, tdClass, thClass, renderResponsive, data }"
+      >
+        <template v-if="renderResponsive">
+          <tbody
+            v-for="(row, rowIndex) in data"
+            :key="rowIndex"
+            :class="[tbodyClass, rowIndex % 2 === 0 ? 'bg-gray-100' : '']"
+          >
+            <tr :class="trClass">
+              <th :class="thClass">
+                Name
+              </th>
+              <td :class="[tdClass, 'relative']">
+                <t-dropdown
+                  :visible-arrow="false"
+                  placement="left-start"
+                  variant="tertiary"
+                  class="absolute right-0 top-0"
+                >
+                  <template slot="button-content">
+                    <svg
+                      version="1.1"
+                      viewBox="0 0 16 16"
+                      class="fill-current text-gray-600 svg-icon svg-fill"
+                      heigth="20"
+                      style="width: 20px;"
+                    ><path
+                      pid="0"
+                      d="M13 7a2 2 0 1 1 .001 3.999A2 2 0 0 1 13 7zM8 7a2 2 0 1 1 .001 3.999A2 2 0 0 1 8 7zM3 7a2 2 0 1 1 .001 3.999A2 2 0 0 1 3 7z"
+                    /></svg>
+                  </template>
+                  <button
+                    class="block hover:text-white text-gray-800 px-4 py-2 hover:bg-blue-500 w-full text-left"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    class="block hover:text-white text-gray-800 px-4 py-2 hover:bg-blue-500 w-full text-left"
+                  >
+                    Delete
+                  </button>
+                </t-dropdown>
+                {{ row.name }}
+              </td>
+            </tr>
+            <tr :class="trClass">
+              <th :class="thClass">
+                Email
+              </th>
+              <td :class="[tdClass, 'td-overflow']">
+                <a
+                  :href="`mailto: ${row.email}`"
+                  class="text-gray-600 hover:underline"
+                >{{ row.email }}</a>
+              </td>
+            </tr>
+            <tr :class="trClass">
+              <th :class="thClass">
+                Status
+              </th>
+              <td :class="[tdClass]">
+                <span
+                  v-if="row.is_approved"
+                  class="d-flex py-1 px-5 bg-green-200 text-green-900 text-sm rounded-full font-bold"
+                >
+                  Active
+                </span>
+                <span
+                  v-else
+                  class="d-flex py-1 px-5 bg-gray-200 text-gray-900 text-sm rounded-full font-bold"
+                >
+                  Inactive
+                </span>
+              </td>
+            </tr>
+          </tbody>
+        </template>
+      </template>
+      <template
+        slot="row"
+        slot-scope="{ trClass, tdClass, rowIndex, row }"
+      >
+        <tr :class="[trClass, rowIndex % 2 === 0 ? 'bg-gray-100' : '']">
+          <td :class="[tdClass, 'w-full']">
+            {{ row.name }}
+          </td>
+          <td :class="tdClass">
+            <a
+              :href="`mailto: ${row.email}`"
+              class="text-gray-600 hover:underline"
+            >{{ row.email }}</a>
+          </td>
+          <td :class="[tdClass, 'text-center']">
+            <span
+              v-if="row.is_approved"
+              class="d-flex py-2 px-5 bg-green-200 text-green-900 text-sm rounded-full font-bold"
+            >
+              Active
+            </span>
+            <span
+              v-else
+              class="d-flex py-2 px-5 bg-gray-200 text-gray-900 text-sm rounded-full font-bold"
+            >
+              Inactive
+            </span>
+          </td>
+          <td :class="[tdClass, 'text-right']">
+            <t-dropdown
+              :visible-arrow="false"
+              placement="bottom-end"
+              variant="tertiary"
+            >
+              <template slot="button-content">
+                <svg
+                  version="1.1"
+                  viewBox="0 0 16 16"
+                  class="fill-current text-gray-600 svg-icon svg-fill"
+                  heigth="20"
+                  style="width: 20px;"
+                ><path
+                  pid="0"
+                  d="M13 7a2 2 0 1 1 .001 3.999A2 2 0 0 1 13 7zM8 7a2 2 0 1 1 .001 3.999A2 2 0 0 1 8 7zM3 7a2 2 0 1 1 .001 3.999A2 2 0 0 1 3 7z"
+                /></svg>
+              </template>
+              <button
+                class="block hover:text-white text-gray-800 px-4 py-2 hover:bg-blue-500 w-full text-left"
+              >
+                Edit
+              </button>
+              <button
+                :to="{ name: 'settings.profile' }"
+                class="block hover:text-white text-gray-800 px-4 py-2 hover:bg-blue-500 w-full text-left"
+              >
+                Delete
+              </button>
+            </t-dropdown>
+          </td>
+        </tr>
+      </template>
+      <template
+        slot="tfoot"
+        slot-scope="{ tfootClass, trClass, tdClass, renderResponsive }"
+      >
+        <tfoot :class="tfootClass">
+          <tr :class="trClass">
+            <td
+              :class="tdClass"
+              :colspan="renderResponsive ? 2 : 4"
+            >
+              <t-pagination
+                :hide-prev-next-controls="renderResponsive"
+                :total-items="100"
+                :per-page="renderResponsive ? 3 : 5"
+                :class="{'ml-auto': !renderResponsive, 'mx-auto': renderResponsive}"
+              />
+            </td>
+          </tr>
+        </tfoot>
+      </template>
+    </t-table>
   </t-card>
 </template>
 
@@ -573,6 +776,17 @@ Vue.use(VueTailwind, {
         wrapper: 'mb-3',
       },
 
+    },
+  },
+  TTable: {
+    classes: {
+      table: 'w-full rounded-lg overflow-hidden table border-blue-700 border',
+      thead: '',
+      theadTr: 'border-blue-700',
+      theadTh: 'uppercase font-bold p-2 bg-blue-500 text-blue-900 text-sm text-shadow',
+      tbody: '',
+      tr: 'border-t-0',
+      td: 'p-2 text-sm text-center bg-blue-400 text-blue-900',
     },
   },
   TButton: {
