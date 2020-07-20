68824d0f55a1615d0004be86171aed8c65f7295a
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-07-20 10:45:37 -0500

    Ignore max-len

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'max-len': 'off',
   },
   overrides: [
     {
