7927959757c37b00ddf3776e95b2aa9d80c88c42
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-08-27 23:04:28 -0500

    Fix typing file

--- vue.config.js
+++ vue.config.js
@@ -5,23 +5,23 @@
  * Code by various users from https://github.com/vuejs/vue-cli/issues/1081
  */
 const fixEmitDeclarationFilesForTypeScript = {
-  chainWebpack: config => {
-    if (process.env.NODE_ENV === "production") {
-      config.module.rule("ts").uses.delete("cache-loader");
+  chainWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.module.rule('ts').uses.delete('cache-loader');
       config.module
-        .rule("ts")
-        .use("ts-loader")
-        .loader("ts-loader")
-        .tap(options => ({
+        .rule('ts')
+        .use('ts-loader')
+        .loader('ts-loader')
+        .tap((options) => ({
           ...options,
           transpileOnly: false,
-          happyPackMode: false
+          happyPackMode: false,
         }));
     }
   },
-  parallel: false
+  parallel: false,
 };
 
 module.exports = {
-  ...fixEmitDeclarationFilesForTypeScript
+  ...fixEmitDeclarationFilesForTypeScript,
 };
