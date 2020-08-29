95cf144a8999081d107c595d6f96260073126eb3
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-08-29 01:12:10 -0500

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,6 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '^~/(.*)$': '<rootDir>/$1',
-  },
-  roots: [
-    '<rootDir>',
-  ],
-  modulePaths: [
-    '<rootDir>',
-  ],
-  moduleDirectories: [
-    'node_modules',
+  moduleFileExtensions: [
+    'ts', 'js',
   ],
 };
