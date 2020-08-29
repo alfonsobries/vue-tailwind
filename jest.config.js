687cae3949c34735ed25b291f72f9cb061e8a01e
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-08-29 01:22:41 -0500

    no idea what to do

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,3 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-  moduleFileExtensions: [
-    'ts', 'js',
-  ],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
 };
