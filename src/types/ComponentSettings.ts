005c946de969d94206f0b91baca44e424c183873
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-09-01 10:32:01 -0500

    Remove comments

--- src/types/ComponentSettings.ts
+++ src/types/ComponentSettings.ts
@@ -1,15 +1,7 @@
-// import ComponentVariants from './ComponentVariants';
-// import CssClasses from './CssClasses';
-// import { Locales, LocaleName, Locale } from './locale';
+import { PropOptions } from 'vue';
 
 type ComponentSettings = {
-  [key: string]: any;
-  // classes?: CssClasses;
-  // fixedClasses?: CssClasses;
-  // wrapped?: boolean;
-  // locales?: Locales
-  // locale?: Locale
-  // lang?: LocaleName
+  [key: string]: PropOptions;
 }
 
 export default ComponentSettings;
