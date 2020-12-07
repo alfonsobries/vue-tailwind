685f388249c7cfe6464827513c672e9b25148a4a
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-12-07 13:28:10 -0600

    Remove extra class

--- src/components/TPagination.ts
+++ src/components/TPagination.ts
@@ -78,7 +78,7 @@ const TPagination = Component.extend({
         return {
           wrapper: 'table border-collapse text-center bg-white mx-auto shadow-sm',
           element: 'w-8 h-8 border border-gray-200 table-cell hover:border-blue-100',
-          activeElement: 'w-8 h-8 border border-gray-200 border-blue-500 table-cell hover:border-blue-600',
+          activeElement: 'w-8 h-8 border border-blue-500 table-cell hover:border-blue-600',
           disabledElement: 'w-8 h-8 border border-gray-200 table-cell',
           ellipsisElement: 'w-8 h-8 border border-gray-200 hidden md:table-cell',
           activeButton: 'bg-blue-500 w-full h-full text-white hover:bg-blue-600 transition duration-100 ease-in-out focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50',
